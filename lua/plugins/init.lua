fe73c09b8724500967681d328fe5e7a8b0c62463
bloq <beringer.owen@gmail.com>
2024-10-08 05:42:14 -0400

    nvChad++

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,16 +10,48 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
+      require "custom.lspconfig"
     end,
   },
 
   -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
+  --   "jose-elias-alvarez/null-ls.nvim",
+  --   event = "VeryLazy",
+  --   opts = function()
+  --     return require "configs.null-ls"
+  --   end,
   -- },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        -- "html",
+        -- "css",
+        -- "bash",
+        -- "html",
+        -- "javascript",
+        -- "json",
+        -- "markdown",
+        -- "markdown_inline",
+        -- "python",
+        -- "query",
+        -- "regex",
+        -- "tsx",
+        -- "typescript",
+        -- "yaml",
+      },
+    },
+  },
+  -- the opts function can also be used to change the default opts:
+  {
+    "nvim-lualine/lualine.nvim",
+    -- event = "Lazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, "😄")
+    end,
+  },
 }
